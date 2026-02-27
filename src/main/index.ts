976ca23d18af4c03bacfb65f333b034816ab0a81
Theon Tang <theon.tang@lego.com>
2026-02-27 20:43:56 +0800

    fix: set macOS menu bar app name to 简历助手

--- src/main/index.ts
+++ src/main/index.ts
@@ -51,6 +51,9 @@ function createWindow(): void {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(async () => {
+  // Set app name for macOS menu bar
+  app.setName('简历助手')
+
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.cv-assistant')
 
